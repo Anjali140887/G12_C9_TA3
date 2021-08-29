be93526e2b488fc4cfa4b69a2657a25e6411309c
Anjali140887 <86819899+Anjali140887@users.noreply.github.com>
2021-08-29 18:55:00 +0530

    Create C9_TA3_template.py

---
+++ C9_TA3_template.py
@@ -0,0 +1,7 @@
+import pygame,sys,random
+pygame.init()
+clock=pygame.time.Clock()
+pygame.display.set_mode((400,600))
+pygame.display.set_caption('Infinite Flying Bird Game')
+while True:
+  pygame.quit()
